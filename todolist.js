536563ae81b8d6f02c969b5c2d19191045bd10d4
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-24 10:05:18 -0700

    todolist update

---
+++ todolist.js
@@ -0,0 +1,33 @@
+var input = document.getElementById("myInput");
+var button = document.getElementById("addbutton");
+var list = document.getElementById("list");
+
+function add() {
+    var toDoItem = document.createElement("div");
+    var toDoText = input.value;
+    var newpElement = document.createElement("p");
+    newpElement.classList.add("items-text");
+    newpElement.innerText = toDoText;
+    var finishButton = document.createElement("button");
+    finishButton.innerText = "Finish";
+    finishButton.classList.add("finish");
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    finishButton.addEventListener("click", function () {
+        newpElement.classList.toggle("finished");
+    });
+
+    deleteButton.addEventListener("click", function () {
+        toDoItem.remove();
+    });
+
+    toDoItem.appendChild(newpElement);
+    toDoItem.appendChild(finishButton);
+    toDoItem.appendChild(deleteButton);
+    list.appendChild(toDoItem);
+    toDoItem.classList.add("todo");
+
+    input.value = ""; // Clear the input field
+}
+
+button.addEventListener("click", add);
